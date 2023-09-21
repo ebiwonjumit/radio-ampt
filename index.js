6449894f03a155798f3c5414f1f539a1ef7b4f1f
Oluwatito Ebiwonjumi <31973148+ebiwonjumit@users.noreply.github.com>
2023-09-21 13:23:14 -0500

    Refreshed with Old Code

--- index.js
+++ index.js
@@ -8,6 +8,22 @@ const api = Router();
 api.get("/hello", (req, res) => {
   return res.status(200).send({ message: "Hello from the public api!" });
 });
+api.get("/greet/:name", (req, res) => {
+  const { name } = req.params;
+
+  if (!name) {
+    return res.status(400).send({ message: "Missing route param for `name`!" });
+  }
+
+  return res.status(200).send({ message: `Hello ${name}!` });
+});
+
+api.post("/submit", async (req, res) => {
+  return res.status(200).send({
+    body: req.body,
+    message: "You just posted data",
+  });
+});
 
 app.use("/api", api);
 
